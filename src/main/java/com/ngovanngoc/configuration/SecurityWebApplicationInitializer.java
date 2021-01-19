e439eb32b90776a1c73b84b6a49e73d2f7fc8849
ngovanngoc2014 <ngovanngoc2014@gmail.com>
2021-01-19 15:22:15 +0700

    updata

---
+++ src/main/java/com/ngovanngoc/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.ngovanngoc.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
